0d419e9bf540f02198ad7e1ecd4bbc49ef182141
dtomeme <124586086+dtomeme@users.noreply.github.com>
2023-02-09 21:07:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ var parseData = function(data){
 var renderData = function(gson) {
   for(var i=0; i<gson.length; i++) {
     var row_data = gson[i];
-    var row_html = ${row_data["formResponse"]};
+    var row_html = `<div class="row"><div class="left"><div  style="width:${row_data["formResponse"]}%"></div></div></div>`;
 console.log(row_html);
   }
 }
