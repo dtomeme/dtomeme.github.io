00b059a35bee9eeb3692f637d6c1ec65fd7acd87
dtomeme <124586086+dtomeme@users.noreply.github.com>
2023-02-12 23:38:26 +0700

    Update index.js

--- a1/index.js
+++ a1/index.js
@@ -50,9 +50,9 @@ var renderData = function(gson) {
       var content = `<main class="container"><div class="heart-1 heart"></div><div class="heart-2 heart"></div><div class="heart-3 heart"></div><div class="heart-4 heart"></div><div class="heart-5 heart"></div><div class="heart-6 heart"></div><div class="heart-7 heart"></div></main><h1>Valentine's Day Quiz</h1><form id="quiz-form"><div id="question"></div><div><input type="radio" id="answer1" name="answer"><label for="answer1" id="answer1-label"></label></div><div><input type="radio" id="answer2" name="answer"><label for="answer2" id="answer2-label"></label></div><div><input type="radio" id="answer3" name="answer"><label for="answer3" id="answer3-label"></label></div><button type="submit">Submit</button></form>`;
       el.innerHTML = content;
       document.getElementById('question').innerHTML = question;
-      document.getElementById('answer1').innerHTML = answer1;
-      document.getElementById('answer2').innerHTML = answer2;
-      document.getElementById('answer3').innerHTML = answer3;
+      document.getElementById('answer1').value = answer1;
+      document.getElementById('answer2').value = answer2;
+      document.getElementById('answer3').value = answer3;
     }
     console.log("url:", url);
     console.log("path:", path);
