0f11c7fc15c0930fdda7b0df2c933ed4ff4d71c6
dtomeme <124586086+dtomeme@users.noreply.github.com>
2023-03-03 20:29:59 -0500

    Update index.js

--- a2/index.js
+++ a2/index.js
@@ -50,10 +50,10 @@ var renderData = function(gson) {
     answer3 = values[6];
     if (url.includes(values[0])) {
       var el = document.getElementById('content');
+      document.getElementById('question').innerHTML = question;
       var content = `<main class="container"><div class="heart-1 heart"></div><div class="heart-2 heart"></div><div class="heart-3 heart"></div><div class="heart-4 heart"></div><div class="heart-5 heart"></div><div class="heart-6 heart"></div><div class="heart-7 heart"></div></main><h1>Valentine's Day Quiz</h1><form id="quiz-form"><div id="question"></div><div><input type="radio" id="answer1" name="answer"><label for="answer1" id="answer1-label"></label></div><div><input type="radio" id="answer2" name="answer"><label for="answer2" id="answer2-label"></label></div><div><input type="radio" id="answer3" name="answer"><label for="answer3" id="answer3-label"></label></div><button type="submit">Submit</button></form>`;
       el.innerHTML = content;
       console.log("question " + question);
-      document.getElementById('question').innerHTML = question;
       document.getElementById('answer1').placeholder = answer1;
       document.getElementById('answer2').placeholder = answer2;
       document.getElementById('answer3').placeholder = answer3;
