562445ec5ead9b9191feb672b4689b5ceb5b51d7
dtomeme <124586086+dtomeme@users.noreply.github.com>
2023-03-04 21:40:54 -0500

    Update index.js

--- a2/index.js
+++ a2/index.js
@@ -1,24 +1,27 @@
-var papaparse_script = document.createElement("script");
-papaparse_script.type = "text/javascript";
-papaparse_script.src = "https://cdn.jsdelivr.net/npm/papaparse@5.3.2/papaparse.min.js";
-document.body.appendChild(papaparse_script);
+function bodyLoad() {
+    var papaparse_script = document.createElement("script");
+    papaparse_script.type = "text/javascript";
+    papaparse_script.src = "https://cdn.jsdelivr.net/npm/papaparse@5.3.2/papaparse.min.js";
+    document.body.appendChild(papaparse_script);
 
-//fetch the data
-var sheet_csv = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSehJHcQEsfNWKn8_SumvGZ-Brnmlyw56iM6koUWFlrbcIH2To5gOTny43TQzbBW0rs78SYFd_8WzAb/pub?output=csv';
-fetch(sheet_csv)
-  .then(function(response){return response.text();})
-  .then(function(data){
-    parseData(data)
-  });
+    //fetch the data
+    var sheet_csv = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSehJHcQEsfNWKn8_SumvGZ-Brnmlyw56iM6koUWFlrbcIH2To5gOTny43TQzbBW0rs78SYFd_8WzAb/pub?output=csv';
+    fetch(sheet_csv)
+      .then(function(response){return response.text();})
+      .then(function(data){
+        parseData(data)
+      });
 
-//parse the data
-var parseData = function(data){
-  var gson = Papa.parse(data, {header:true}).data;
-  renderData(gson);
-};
+    //parse the data
+    var parseData = function(data){
+      var gson = Papa.parse(data, {header:true}).data;
+      renderData(gson);
+    };
+}
 
 var renderData = function(gson) {
   const url = window.location.href;
+  let path = "";
   let question = "";
   let answer1 = "";
   let answer2 = "";
@@ -30,8 +33,8 @@ var renderData = function(gson) {
     let row_data = gson[k];
     let row_html = row_data["formResponse"];
     console.log(row_html);
-    values[0] = row_html.substring(row_html.indexOf(vars[0])+6,row_html.indexOf(vars[0])+8);
-    if (isNaN(values[0].charAt(0))) {
+    path = row_html.substring(row_html.indexOf(vars[0])+6,row_html.indexOf(vars[0])+8);
+    if (isNaN(path.charAt(0))) {
       i = 0;
     }
     for (var j = 0; j < 8; j++) {
@@ -49,14 +52,16 @@ var renderData = function(gson) {
     answer2 = values[5];
     answer3 = values[6];
     if (url.includes(values[0].substring(0,2))) {
-      console.log(typeof values[0] + " " + values[0].substring(0,2));
-      console.log("def");
       var el = document.getElementById('content');
-      var content = `<main class="container"><div class="heart-1 heart"></div><div class="heart-2 heart"></div><div class="heart-3 heart"></div><div class="heart-4 heart"></div><div class="heart-5 heart"></div><div class="heart-6 heart"></div><div class="heart-7 heart"></div></main><h1>Valentine's Day Quiz</h1><form id="quiz-form"><div id="question" style="font-size:200%;"><b>` + question + `</b></div><div><input type="radio" id="answer1" name="answer"><label for="answer1" id="answer1-label">` + answer1 + `</label></div><div><input type="radio" id="answer2" name="answer"><label for="answer2" id="answer2-label">` + answer2 + `</label></div><div><input type="radio" id="answer3" name="answer"><label for="answer3" id="answer3-label">` + answer3 + `</label></div><button type="submit">Submit</button></form>`;
+      var content = `<main class="container"><div class="heart-1 heart"></div><div class="heart-2 heart"></div><div class="heart-3 heart"></div><div class="heart-4 heart"></div><div class="heart-5 heart"></div><div class="heart-6 heart"></div><div class="heart-7 heart"></div></main><h1>Valentine's Day Quiz</h1><form id="quiz-form"><div id="question" style="font-size:200%;"><b>` + question + `</b></div><div><input type="radio" id="answer1" name="answer"><label for="answer1" id="answer1-label">` + answer1 + `</label></div><div><input type="radio" id="answer2" name="answer"><label for="answer2" id="answer2-label">` + answer2 + `</label></div><div><input type="radio" id="answer3" name="answer"><label for="answer3" id="answer3-label">` + answer3 + `</label></div><button type="submit" onclick="checkSelect()">Submit</button></form>`;
       el.innerHTML = content;
     }
     else {
       console.log("abc");
     }
   }
+}
+
+function checkSelect() {
+  
 }
